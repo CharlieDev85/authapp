f9b17fe1815125498201e6ccbaf8c60e9a6224a1
CharlieDev <php.developer.charlie@gmail.com>
2017-04-16 14:44:15 -0600

    register and thanks templates were added, tested and they worked

--- app/views/thanks.blade.php
+++ app/views/thanks.blade.php
@@ -24,22 +24,11 @@
 </header>
 <div id="contentWrapper">
   <article id="mainContent">
-    <h1>Sign Up!</h1>
+    <h1>You signed up</h1>
     <article class="post">
-      <h2>New User Registration</h2>
+      <h2>Thanks for registering</h2>
       <!-- form goes here -->
-        {{ Form::open(array('url'=>'register'))}}
-        {{ Form::label('email', 'Email Address') }}
-        {{ Form::text('email') }}
-
-        {{ Form::label('username', 'Username') }}
-        {{ Form::text('username') }}
-
-        {{ Form::label('password', 'Password') }}
-        {{ Form::password('password') }}
-
-        {{ Form::submit('Sign Up') }}
-        {{ Form::close() }}
+    <p>You have registered {{ $theEmail }}.</p>
 </article>
   </article>
   <aside>
